fbba69ab8be89c0a84697c1dd732ddbce0ccdc84
Britton <britton.wolfe1999@gmail.com>
2020-06-30 14:54:53 -0400

    started making viewsets and urls:

---
+++ cardclub/rest.py
@@ -0,0 +1,21 @@
+from rest_framework import routers, serializers, viewsets
+
+from .models import Card
+
+router = routers.DefaultRouter
+
+class CardSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Card
+		fields = [
+			'id',
+			'url'
+		]
+class CardViewSet(viewsets.ModelViewSet):
+	queryset = Card.objects.all()
+	serializer_class = CardSerializer
+	def perform_create(self, serializer):
+		serializer.save(author = self.request.user)
+	def get_queryset(self):
+		return queryset.filter(user = self.request.user)
+router.register('card', CardViewSet)
--- cardclub/urls.py
+++ cardclub/urls.py
@@ -17,15 +17,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
 
+from .rest import router
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('api/auth/', include('djoser.urls')),
+	path('api/auth/', include('djoser.urls.authtoken')),
+	path('api/', include(router.urls))
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns

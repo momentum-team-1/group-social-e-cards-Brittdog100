bf56c9e6a7ead8ec4a5108ac61d03791514e7379
Britton W <britton.wolfe1999@gmail.com>
2020-07-09 10:47:45 -0400

    turned off debug mode and enabled proper CORS settings.

--- cardclub/settings.py
+++ cardclub/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -156,5 +156,5 @@ if DEBUG:
 	CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-	# add ashley's app here
+	"cardclub.netlify.app"
 ]
